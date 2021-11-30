b18858727636a461f8c07437c454c0d4e071600e
Michael Albano <maalbano@gmail.com>
2021-11-30 13:34:20 +0800

    96day PTBC 2nd draft deployed

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "84e9934a6f570c5236d3a011f585e53c",
-"index.html": "a66c48aeb68b67b22f3ea7da6ded6a6e",
-"/": "a66c48aeb68b67b22f3ea7da6ded6a6e",
-"main.dart.js": "905e0ea0515b77965c85c04caa6fae71",
+"index.html": "6e82be52403cc11793e101d6cd002a50",
+"/": "6e82be52403cc11793e101d6cd002a50",
+"main.dart.js": "97b4da86be2aa7cebd271c310775392d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
